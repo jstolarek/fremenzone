963ae63dcb5f8943b12821e3ac93350b02ce68c5
Jan Stolarek <jan.stolarek@mailbox.org>
2024-03-05 20:02:40 +0100

    Aktualizacja 5 marca 2024
      * Dodana recenzja Dune: Spice Wars
      * Dodany dział Gadżety
        - recenzja ornitoptera z Lego
        - recenzja talii kart od theory11
      * Dodana własna strona błędu 404
      * Optymalizacja czcionek sieciowych
      * Drobne porządki w recenzjach

--- tidy-html-check.sh
+++ tidy-html-check.sh
@@ -3,13 +3,24 @@
 EXIT_CODE=0
 
 # Check all HTML files in the docs/ directory, except for the old site files in
-# docs/kegacy.  These are known to be trash.
+# docs/legacy.  These are known to be trash.
 for i in `find docs -name "*.html" -not -path "docs/legacy/*"`; do
   RESULT=`tidy -errors -quiet $i 2>&1`
 
   if [[ $RESULT ]]; then
     echo -e $i
     tidy -errors -quiet $i
+    EXIT_CODE=1
+  fi
+done
+
+# Do the same with XML files
+for i in `find docs -name "*.xml" -not -path "docs/legacy/*"`; do
+  RESULT=`tidy -xml -errors -quiet $i 2>&1`
+
+  if [[ $RESULT ]]; then
+    echo -e $i
+    tidy -xml -errors -quiet $i
     EXIT_CODE=1
   fi
 done
