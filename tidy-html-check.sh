3790b5ad79878b1ac6a978967d2a9c724016050e
Jan Stolarek <jwstolarek@gmail.com>
2024-03-31 21:48:49 +0200

    Użycie ścieżek bezwzględnych w skrypcie sprawdzającym HTML

--- tidy-html-check.sh
+++ tidy-html-check.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 EXIT_CODE=0
+DIR=`pwd`
 
 # Check all HTML files in the docs/ directory, except for the old site files in
 # docs/legacy.  These are known to be trash.
-for i in `find docs -name "*.html" -not -path "docs/legacy/*"`; do
+for i in `find $DIR/docs -name "*.html" -not -path "$DIR/docs/legacy/*"`; do
   RESULT=`tidy -errors -quiet $i 2>&1`
 
   if [[ $RESULT ]]; then
@@ -15,7 +16,7 @@ for i in `find docs -name "*.html" -not -path "docs/legacy/*"`; do
 done
 
 # Do the same with XML files
-for i in `find docs -name "*.xml" -not -path "docs/legacy/*"`; do
+for i in `find $DIR/docs -name "*.xml" -not -path "$DIR/docs/legacy/*"`; do
   RESULT=`tidy -xml -errors -quiet $i 2>&1`
 
   if [[ $RESULT ]]; then
